b47c9554ddda18fce6d68509f08d57dee306e460
Arlen-Augusto-Pereira <arlenaugustop@gmail.com>
2026-05-14 20:55:41 -0400

    implementação tela home novo botão

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,54 @@
-import {Text, View } from "react-native";
+import {Text, View, StyleSheet } from "react-native";
 import {SafeAreaView } from "react-native-safe-area-context";
-
+import { THEME } from "../styles/constants";
+import {Button} from "../componentes/Buttons";
 
 export default function Homescreen(){
   return (
     <SafeAreaView>
-       <View>
-          <Text>Home Screan</Text>
-        </View>
+       <View style= {styles.container}>
+          <View style={styles.headerContainer}>
+            <Text style={styles.logo}>DevCard</Text>
+            <Text  style={styles.subtitle}>Seu cartão de visita digital de dev mobile</Text>
+         </View>
+             
+            <Button label="Criar meu cartão"/>
+
+       </View>
+
       </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+    height: "100%",
+    justifyContent: "center",
+    alignContent: "center",
+    paddingHorizontal: 24
+  },
+
+  headerContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+    height: 750
+  },
+
+  logo: {
+    color: THEME.colors.primary,
+    fontWeight: "bold",
+    fontSize: 56,
+  },
+
+  subtitle: {
+    color: "#747477",
+    fontWeight: "400",
+    fontSize: 16,
+    width: 200,
+    textAlign: "center"
+  },
+
+});
