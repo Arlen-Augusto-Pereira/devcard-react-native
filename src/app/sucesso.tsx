9f95be8cd4eb330af7515219747b3ac879d05374
Arlen-Augusto-Pereira <arlenaugustop@gmail.com>
2026-05-15 18:48:16 -0400

    implementção da tela de sucesso

--- src/app/sucesso.tsx
+++ src/app/sucesso.tsx
@@ -1,13 +1,72 @@
-import {Text, View } from "react-native";
+import {Text, View, StyleSheet } from "react-native";
 import {SafeAreaView } from "react-native-safe-area-context";
-
+import {Button} from "../componentes/Buttons";
+import { THEME } from "../styles/constants";
 
 export default function Sucessoscreen(){
   return (
     <SafeAreaView>
-       <View>
-          <Text>Home Screan</Text>
-        </View>
+       <View style= {styles.container}>
+          <View style={styles.headerContainer}>
+            <Text style={styles.title}>
+              Cartão criado com sucesso!
+            </Text>
+            <Text  style={styles.subtitle}>
+              Seu cartão de visita digital está pronto.
+              Compatilhe com o pessoal! </Text>
+         </View>
+
+         <View style={styles.footerContainer}>   
+            <Button label="Criar outro cartão"/>
+            <Button label="Voltar ao início" variant="secondary"/>
+          </View>
+
+       </View>
+
       </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "column",
+    height: "100%",
+    justifyContent: "center",
+    alignContent: "center",
+    paddingHorizontal: 24
+  },
+
+  headerContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+    alignContent: "center",
+    alignItems: "center",
+    height: 750,
+    gap: 12
+  },
+
+  title: {
+
+    color: THEME.colors.heading,
+    fontWeight: "bold",
+    fontSize: THEME.text.heading.h2,
+    width: 200,
+    textAlign: "center"
+
+    
+  },
+
+  subtitle: {
+    color: THEME.colors.subtitle,
+    fontWeight: "400",
+    fontSize: 16,
+    width: 200,
+    textAlign: "center"
+  },
+  footerContainer: {
+  flexDirection: "column",
+  gap: 12
+
+  }
+
+});
--- src/componentes/Buttons/styles.ts
+++ src/componentes/Buttons/styles.ts
@@ -1,7 +1,7 @@
 import { StyleSheet } from "react-native";
 import { THEME } from "../../styles/constants";
 
-export const styles = StyleSheet.create({
+export const stylePrimary = StyleSheet.create({
   buttonContainer: {
     backgroundColor: THEME.colors.primary,
     flexDirection: "row",
@@ -11,9 +11,44 @@ export const styles = StyleSheet.create({
     paddingHorizontal: 24,
     borderRadius: THEME.border.radius.md,
   },
+
   buttonText: {
     color: THEME.colors.primary_foreground,
     fontWeight: "500",
     fontSize: 18,
   },
-});
+})
+
+const styleSecondary = StyleSheet.create({
+  buttonContainer: {
+    ...stylePrimary.buttonContainer,
+    backgroundColor: "transparent"
+    
+  },
+  buttonText: {
+    ...stylePrimary.buttonText,
+    color: THEME.colors.primary
+  },
+
+})
+
+const styleOutline = StyleSheet.create({
+  buttonContainer: {
+    ...stylePrimary.buttonContainer,
+    backgroundColor: "transparent",
+    borderColor: THEME.colors.primary,
+    borderWidth: 2
+    
+  },
+  buttonText: {
+    ...stylePrimary.buttonText,
+    color: THEME.colors.primary
+  },
+
+})
+
+export const styles = {
+    primary: stylePrimary,
+    secondary: styleSecondary,
+    outline: styleOutline
+ } as const
